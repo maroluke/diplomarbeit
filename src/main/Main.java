49cbf4b4d505fada228a478cd804b06e72b77f41
maroluke <web@lukacmarko.com>
2016-12-23 19:17:45 +0100

    Main Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,4 +4,8 @@ package main;
  * Created by markolukac on 23.12.16.
  */
 public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+    }
 }
